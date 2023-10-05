92e4a2e01bad5245f7a26e2d81d7ec8b553e51bc
Thinh Duong <tanthinhdt24@gmail.com>
2023-10-05 09:30:04 +0700

    chore: update slicer processor and slicing task

--- vlr/data/processors/slicer.py
+++ vlr/data/processors/slicer.py
@@ -1,14 +1,11 @@
-import io
 import os
-import contextlib
 import moviepy.editor as mp
 from vlr.data.processors.base import Processor
 
 
 class Slicer(Processor):
     def __init__(
-        self, raw_dir: str,
-        visual_dir: str,
+        self, visual_dir: str,
         audio_dir: str,
         fps: int = 25,
         duration_threshold: float = 1.0,
@@ -18,7 +15,6 @@ class Slicer(Processor):
         overwrite: bool = False,
     ):
         """
-        :param raw_dir:             Path to directory with raw video files.
         :param visual_dir:          Path to directory with muted video files.
         :param audio_dir:           Path to directory with sound files.
         :param fps:                 Frame rate.
@@ -28,7 +24,6 @@ class Slicer(Processor):
         :param keep_last_segment:   Keep last video segment.
         :param overwrite:           Overwrite existing files.
         """
-        self.raw_dir = raw_dir
         self.visual_dir = visual_dir
         self.audio_dir = audio_dir
         self.fps = fps
@@ -37,7 +32,6 @@ class Slicer(Processor):
         self.segment_overlap = segment_overlap
         self.keep_last_segment = keep_last_segment
         self.overwrite = overwrite
-        self.output_buffer = io.StringIO()
 
     def process(self, batch: dict):
         """
@@ -48,66 +42,52 @@ class Slicer(Processor):
         processed_batch = {
             "file": [],
             "visual": [],
-            "fps": [],
             "audio": [],
         }
-        for file in batch["file"]:
-            file_id = file.split('.')[0]
-            raw_video_path = os.path.join(self.raw_dir, file)
+        for raw_video_path in batch["file"]:
+            file_id = os.path.basename(raw_video_path).split('.')[0]
 
             try:
-                # Omit what is printed to stdout.
-                with contextlib.redirect_stdout(self.output_buffer):
-                    video = mp.VideoFileClip(raw_video_path)
-                    duration = video.duration
+                video = mp.VideoFileClip(raw_video_path)
+                duration = video.duration
 
-                    if duration < self.duration_threshold:
-                        video.close()
-                        raise Exception
-
-                    video = video.set_fps(self.fps)
-                    # Split video into segments.
-                    start = 0
-                    end = self.segment_duration
-                    segment_id = f"{file_id}" + "-{start}-{end}"
-                    audio_path = os.path.join(self.audio_dir, segment_id + ".wav")
-                    visual_path = os.path.join(self.visual_dir, segment_id + ".mp4")
-                    while end <= duration:
-                        segment_visual_path = visual_path.format(start=int(start), end=int(end))
-                        keep_visual = os.path.exists(segment_visual_path) and not self.overwrite
-                        segment_audio_path = audio_path.format(start=int(start), end=int(end))
-                        keep_audio = os.path.exists(segment_audio_path) and not self.overwrite
-                        self.separate(
-                            segment=video.subclip(start, end),
-                            visual_path=segment_visual_path,
-                            keep_visual=keep_visual,
-                            audio_path=segment_audio_path,
-                            keep_audio=keep_audio,
-                        )
-                        processed_batch["file"].append(file)
-                        processed_batch["visual"].append(segment_visual_path)
-                        processed_batch["fps"].append(self.fps)
-                        processed_batch["audio"].append(segment_audio_path)
-                        start += self.segment_duration - self.segment_overlap
-                        end = start + self.segment_duration
-                    end = duration
-                    if end - start >= self.duration_threshold and self.keep_last_segment:
-                        segment_visual_path = visual_path.format(start=int(start), end=int(end))
-                        keep_visual = os.path.exists(segment_visual_path) and not self.overwrite
-                        segment_audio_path = audio_path.format(start=int(start), end=int(end))
-                        keep_audio = os.path.exists(segment_audio_path) and not self.overwrite
-                        self.separate(
-                            segment=video.subclip(start, end),
-                            visual_path=segment_visual_path,
-                            keep_visual=keep_visual,
-                            audio_path=segment_audio_path,
-                            keep_audio=keep_audio,
-                        )
-                        processed_batch["file"].append(file)
-                        processed_batch["visual"].append(segment_visual_path)
-                        processed_batch["fps"].append(self.fps)
-                        processed_batch["audio"].append(segment_audio_path)
+                if duration < self.duration_threshold:
                     video.close()
+                    raise Exception
+
+                video = video.set_fps(self.fps)
+                # Split video into segments.
+                start = 0
+                end = self.segment_duration
+                segment_id = f"{file_id}" + "-{start}-{end}"
+                audio_path = os.path.join(self.audio_dir, segment_id + ".wav")
+                visual_path = os.path.join(self.visual_dir, segment_id + ".mp4")
+                while end - start >= self.duration_threshold and end <= duration:
+                    segment_visual_path = visual_path.format(start=int(start), end=int(end))
+                    segment_audio_path = audio_path.format(start=int(start), end=int(end))
+                    self.separate(
+                        segment=video.subclip(start, end),
+                        visual_path=segment_visual_path,
+                        audio_path=segment_audio_path,
+                    )
+                    processed_batch["file"].append(os.path.basename(segment_visual_path))
+                    processed_batch["visual"].append(segment_visual_path)
+                    processed_batch["audio"].append(segment_audio_path)
+                    start += self.segment_duration - self.segment_overlap
+                    end = start + self.segment_duration
+                end = duration
+                if end - start >= self.duration_threshold and self.keep_last_segment:
+                    segment_visual_path = visual_path.format(start=int(start), end=int(end))
+                    segment_audio_path = audio_path.format(start=int(start), end=int(end))
+                    self.separate(
+                        segment=video.subclip(start, end),
+                        visual_path=segment_visual_path,
+                        audio_path=segment_audio_path,
+                    )
+                    processed_batch["file"].append(os.path.basename(segment_visual_path))
+                    processed_batch["visual"].append(segment_visual_path)
+                    processed_batch["audio"].append(segment_audio_path)
+                video.close()
             except Exception as e:
                 print(e)
                 continue
@@ -117,9 +97,7 @@ class Slicer(Processor):
     def separate(
         self, segment: mp.VideoFileClip,
         visual_path: str,
-        keep_visual: bool,
         audio_path: str,
-        keep_audio: bool,
     ):
         """
         Separate video into audio and visual.
@@ -127,7 +105,15 @@ class Slicer(Processor):
         :param visual_path:  Path to visual file.
         :param audio_path:  Path to audio file.
         """
-        if not keep_visual:
-            segment.without_audio().write_videofile(visual_path, codec="libx264")
-        if not keep_audio:
-            segment.audio.write_audiofile(audio_path, codec="pcm_s16le")
+        if self.overwrite or not os.path.exists(visual_path):
+            segment.without_audio().write_videofile(
+                visual_path,
+                codec="libx264",
+                logger=None,
+            )
+        if self.overwrite or not os.path.exists(audio_path):
+            segment.audio.write_audiofile(
+                audio_path,
+                codec="pcm_s16le",
+                logger=None,
+            )
--- vlr/data/tasks/slice.py
+++ vlr/data/tasks/slice.py
@@ -3,13 +3,17 @@ import sys
 
 sys.path.append(os.getcwd())
 
+import glob
+import shutil
 from dataclasses import dataclass
 from logging import getLogger
-from datasets import Dataset
+from datasets import Dataset, load_from_disk, disable_caching
+from tqdm import tqdm
 from vlr.data.processors.slicer import Slicer
 
 
 logger = getLogger()
+disable_caching()
 
 
 @dataclass
@@ -17,49 +21,124 @@ class Args:
     """
     Data processing arguments.
     """
-    raw_dir = "/mnt/d/Projects/sandboxes/vlr/raw"
+    prev_stage_dir = "/mnt/d/Projects/sandboxes/vlr/raw"
+    cur_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_1"
+    channel_names_path = "/mnt/d/Projects/sandboxes/vlr/channels.txt"
     visual_dir = "/mnt/d/Projects/sandboxes/vlr/visual"
     audio_dir = "/mnt/d/Projects/sandboxes/vlr/audio"
-    save_dir = "/mnt/d/Projects/sandboxes/vlr/v0.2"
-    fps = 25
+    cache_dir = "~/.cache/huggingface/datasets/generator"
     duration_threshold = 1.0
     batch_size = 100
     num_proc = 8
-    num_shards = 10
+    overwrite = False
 
     separator = Slicer(
-        raw_dir=raw_dir,
         visual_dir=visual_dir,
         audio_dir=audio_dir,
-        fps=fps,
+        fps=25,
         duration_threshold=1.0,
-        segment_duration=5.0,
+        segment_duration=3.0,
         segment_overlap=1.0,
         keep_last_segment=True,
         overwrite=False,
     )
 
 
+def get_paths(batch: dict, channel_names_path: str):
+    """
+    """
+    raw_dir = batch["file"][0]
+    paths = []
+    with open(channel_names_path, "r") as f:
+        channel_names = f.read().splitlines()
+    for channel_name in channel_names:
+        channel_dir = os.path.join(raw_dir, channel_name)
+        chunk_dirs = glob.glob(os.path.join(channel_dir, "*"))
+        for chunk_dir in chunk_dirs:
+            video_paths = glob.glob(os.path.join(chunk_dir, "pyactive", "*.avi"))
+            for video_path in video_paths:
+                # yield {"file": video_path}
+                paths.append(video_path)
+    batch["file"] = paths
+    return batch
+
+
 def main(args: Args):
     """
     Main function.
     """
+    # Prepare save directory.
+    if args.overwrite:
+        logger.info("Removing old files...")
+        visual_paths = glob.glob(os.path.join(args.visual_dir, "*"))
+        progress_bar = tqdm(
+            visual_paths,
+            total=len(visual_paths),
+            desc="Removing old visual files",
+            unit="file",
+        )
+        for file_path in progress_bar:
+            os.remove(file_path)
+        audio_paths = glob.glob(os.path.join(args.audio_dir, "*"))
+        progress_bar = tqdm(
+            audio_paths,
+            total=len(audio_paths),
+            desc="Removing old audio files",
+            unit="file",
+        )
+        for file_path in progress_bar:
+            os.remove(file_path)
+
     # Get dataset.
     logger.info("Preparing dataset...")
-    dataset = Dataset.from_dict({"file": os.listdir(args.raw_dir)})
+    # dataset = Dataset.from_generator(
+    #     generator=get_paths,
+    #     gen_kwargs={
+    #         "raw_dir": args.raw_dir,
+    #         "channel_names_path": args.channel_names_path
+    #     },
+    #     num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+    #     cache_dir=None
+    # )
+    dataset = Dataset.from_dict({
+        "file": [args.prev_stage_dir]
+    })
+    dataset = dataset.map(
+        get_paths,
+        batched=True,
+        batch_size=args.batch_size,
+        num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+        load_from_cache_file=False,
+        fn_kwargs={"channel_names_path": args.channel_names_path}
+    )
 
     # Extract audio and visual.
     logger.info("Extract audio and visual from dataset...")
-    print("Before:", len(dataset["file"]))
+    print("Number of samples before slicing:", len(dataset["file"]))
     dataset = dataset.map(
         args.separator.process,
         batched=True, batch_size=args.batch_size,
-        num_proc=args.num_proc if args.num_proc <= os.cpu_count() else os.cpu_count(),
+        num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
     )
-    print("After:", len(dataset["file"]))
-    for index in range(args.num_shards):
-        shard = dataset.shard(args.num_shards, index)
-        shard.save_to_disk(os.path.join(args.save_dir, f"shard_{index}"))
+    print("Number of samples after slicing:", len(dataset["file"]))
+
+    # Save dataset.
+    cur_stage_versions = sorted(os.listdir(args.cur_stage_dir))
+    cur_stage_prev_version = 0 if len(cur_stage_versions) == 0 else int(cur_stage_versions[-1][1])
+    if not args.overwrite:
+        cur_stage_prev_version_dir = os.path.join(args.cur_stage_dir, f"v{cur_stage_prev_version}")
+        old_dataset = load_from_disk(cur_stage_prev_version_dir)
+        file_names = old_dataset.unique("file")
+        for sample in tqdm(dataset, desc="Removing duplicates", unit="sample", total=len(dataset)):
+            if sample["file"] not in file_names:
+                old_dataset = old_dataset.add_item(sample)
+        dataset = old_dataset
+    cur_stage_cur_version = cur_stage_prev_version + 1
+    cur_stage_cur_version_dir = os.path.join(args.cur_stage_dir, f"v{cur_stage_cur_version}")
+    dataset.save_to_disk(
+        cur_stage_cur_version_dir,
+        num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+    )
 
 
 if __name__ == "__main__":
