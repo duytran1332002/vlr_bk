724ac5379cac247a3e232570a4242a4c4fde8f1f
Thinh Duong <tanthinhdt24@gmail.com>
2023-10-06 23:59:42 +0700

    chore: update transcriber and transcribing task
    - write code in the channel-oriented way

--- vlr/data/processors/transcriber.py
+++ vlr/data/processors/transcriber.py
@@ -1,15 +1,12 @@
 import os
-import zipfile
-import torch
 import kenlm
-import logging
+import torch
+import torchaudio
+import zipfile
 import numpy as np
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 from pyctcdecode import Alphabet, BeamSearchDecoderCTC, LanguageModel
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 from vlr.data.processors.base import Processor
-
-
-logger = logging.getLogger("__name__")
 
 
 class Transcriber(Processor):
@@ -21,14 +18,23 @@ class Transcriber(Processor):
         model_path: str,
         lm_gram_name: str = "vi_lm_4grams.bin.zip",
         device: str = "cuda",
+        transcript_dir: str = None,
+        overwrite: bool = False,
     ):
+        """
+        :param model_path:          Path to model.
+        :param lm_gram_name:        Language model name.
+        :param device:              Device to use.
+        :param transcript_dir:      Path to directory containing transcripts.
+        :param overwrite:           Overwrite existing files.
+        """
         # Load the model and the processor.
         self.processor = Wav2Vec2Processor.from_pretrained(model_path)
         self.model = Wav2Vec2ForCTC.from_pretrained(model_path)
 
         # Prepare device.
         if device == "cuda" and not torch.cuda.is_available():
-            logger.warning("No cuda is detected. Use CPU instead.")
+            print("CUDA is not available. Using CPU instead.")
             device = "cpu"
         self.device = device
         self.model = self.model.to(device)
@@ -37,10 +43,12 @@ class Transcriber(Processor):
         lm_zip_path = os.path.join(model_path, lm_gram_name)
         lm_path = lm_zip_path[:-4]
         if not os.path.exists(lm_path):
-            logger.info("Extracting language model...")
             with zipfile.ZipFile(lm_zip_path, 'r') as zip_ref:
                 zip_ref.extractall(model_path)
         self.lm = self.get_lm_decoder(lm_path)
+
+        self.transcript_dir = transcript_dir
+        self.overwrite = overwrite
 
     def get_lm_decoder(self, lm_path: str):
         """
@@ -70,25 +78,66 @@ class Transcriber(Processor):
                                        language_model=LanguageModel(lm_model))
         return decoder
 
-    def process(self, batch: dict):
+    def process_sample(self, sample: dict, channel_name: str):
         """
-        Transcribe and include time offset for each word or sentence.
-        :param sample:  audio sample.
-        :return:        processed sample.
+        Transcribe for a sample.
+        :param sample:          Audio sample.
+        :param channel_name:    Channel name.
+        :return:                Sample with path to transcript.
         """
-        transcripts = []
-        for audio_array, sampling_rate in zip(batch["audio"], batch["sampling_rate"]):
-            audio_array = np.array(audio_array).reshape(-1, 1)
-            # Normalize waveform in order to make it comparable with the pretrained model.
-            if len(audio_array.shape) == 1:
-                audio_array = audio_array[:, np.newaxis]
-            audio_array = np.mean(audio_array, axis=1)
+        id = sample["id"]
+        transcript_path = os.path.join(self.transcript_dir, channel_name, f"{id}.txt")
 
-            transcripts.append(self.transcribe(
-                audio_array=audio_array,
+        if self.overwrite or not os.path.exists(transcript_path):
+            audio_array, sampling_rate = torchaudio.load(sample["audio"]["path"])
+
+            transcript = self.transcribe(
+                audio_array=audio_array.numpy(),
                 sampling_rate=sampling_rate,
-            ))
-        batch["transcript"] = transcripts
+            )
+
+            if len(transcript) > 0:
+                with open(transcript_path, "w") as f:
+                    print(transcript, file=f)
+            else:
+                transcript_path = None
+
+        sample["audio"]["path"] = os.path.basename(sample["audio"]["path"])
+        sample["transcript"] = transcript_path
+        return sample
+
+    def process_batch(self, batch: dict, channel_name: str):
+        """
+        Transcribe a batch of samples.
+        :param sample:          Batch of audio samples.
+        :param channel_name:    Channel name.
+        :return:                Processed batch.
+        """
+        audio_arrays = []
+        for audio in batch["audio"]:
+            audio_array, sampling_rate = torchaudio.load(audio["path"])
+            audio_arrays.append(audio_array.numpy())
+        audio_array = np.stack(audio_arrays, axis=0)
+        sampling_rate = batch["audio"][0]["sampling_rate"]
+
+        transcripts = self.transcribe(
+            audio_array=audio_array,
+            sampling_rate=sampling_rate,
+        )
+        print(transcripts)
+
+        transcript_paths = []
+        for i, audio in enumerate(batch["audio"]):
+            id = batch["id"][i]
+            transcript_path = os.path.join(self.transcript_dir, channel_name, f"{id}.txt")
+
+            if self.overwrite or not os.path.exists(transcript_path):
+                with open(transcript_path, "w") as f:
+                    print(transcripts[i], file=f)
+
+            transcript_paths.append(transcript_path)
+            batch["audio"][i]["path"] = os.path.basename(audio["path"])
+        batch["transcript"] = transcript_paths
         return batch
 
     def transcribe(
@@ -101,8 +150,7 @@ class Transcriber(Processor):
         :param audio_array:     audio array.
         :param sampling_rate:   sampling rate.
         :param beam_width:      beam width.
-        :param align:           align transcript with audio or not.
-        :return:                transcript.
+        :return:                transcript(s).
         """
         if self.device == "cuda":
             torch.cuda.empty_cache()
@@ -110,15 +158,28 @@ class Transcriber(Processor):
         input_values = self.processor(
             audio_array,
             sampling_rate=sampling_rate,
-            return_tensors="pt"
+            return_tensors="pt",
+            padding=True,
         ).input_values.to(self.device)
+        logits = self.model(input_values).logits
 
-        logits = self.model(input_values).logits[0]
+        if logits.shape[0] > 1:
+            transcripts = []
+            for i in range(logits.shape[0]):
+                print(logits.shape)
+                print(logits[i].unsqueeze(0).shape)
+                transcript = self.lm.decode(
+                    logits[i].unsqueeze(0).cpu().detach().numpy(),
+                    beam_width=beam_width
+                )
+                transcripts.append(transcript)
+            return transcripts
         return self.lm.decode(
-            logits.cpu().detach().numpy(), beam_width=beam_width
+            logits[0].cpu().detach().numpy(),
+            beam_width=beam_width
         )
 
 
 if __name__ == "__main__":
     stt = Transcriber()
-    stt.process(r"I:\My Drive\north.wav")
+    stt.process_batch(r"I:\My Drive\north.wav")
--- vlr/data/tasks/transcribe.py
+++ vlr/data/tasks/transcribe.py
@@ -3,10 +3,11 @@ import sys
 
 sys.path.append(os.getcwd())
 
-import glob
 from dataclasses import dataclass
 from logging import getLogger
 from datasets import load_from_disk
+from tqdm import tqdm
+from vlr.data.utils import clean_up
 from vlr.data.processors.transcriber import Transcriber
 
 
@@ -18,16 +19,25 @@ class Args:
     """
     Data processing arguments.
     """
-    data_dir = "/mnt/d/Projects/sandboxes/vlr/v0.3"
-    save_dir = "/mnt/d/Projects/sandboxes/vlr/v0.4"
-    batch_size = 100
+    # Path to directory of previous stage.
+    prev_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_2"
+    # Path to directory pf current stage.
+    cur_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_3"
+    # Path to directory containing denoised sound files.
+    transcript_dir = "/mnt/d/Projects/sandboxes/vlr/transcripts"
+    # Path to file containing channel names.
+    channel_names_path = "/mnt/d/Projects/sandboxes/vlr/channels.txt"
+
+    batch_size = 10
     num_proc = 8
-    overwrite = True
+    overwrite = False
 
     transcriber = Transcriber(
         model_path="/mnt/d/Projects/vlr/vlr/data/resources/wav2vec2-base-vietnamese-250h",
         lm_gram_name="vi_lm_4grams.bin.zip",
         device="cuda",
+        transcript_dir=transcript_dir,
+        overwrite=overwrite,
     )
 
 
@@ -35,19 +45,47 @@ def main(args: Args):
     """
     Main function.
     """
-    logger.info("Transcribing audio...")
-    shard_paths = glob.glob(os.path.join(args.data_dir, "shard_*"))
-    for index, shard_path in enumerate(shard_paths):
-        save_path = os.path.join(args.save_dir, f"shard_{index}")
-        if os.path.exists(save_path) and not args.overwrite:
+    with open(args.channel_names_path, "r") as f:
+        channel_names = f.read().splitlines()
+
+    for channel_name in tqdm(
+        channel_names,
+        desc="Processing channels",
+        total=len(channel_names),
+        unit="channel"
+    ):
+        # Prepare save directory.
+        logger.info("Cleaning up old directories...")
+        clean_up(channel_name, [args.transcript_dir], args.overwrite)
+
+        # Get dataset.
+        logger.info("Preparing dataset...")
+        prev_stage_dir = os.path.join(args.prev_stage_dir, channel_name)
+        if not os.path.exists(prev_stage_dir):
+            print(f"Channel {channel_name} does not exist.")
             continue
-        shard = load_from_disk(shard_path)
-        shard = shard.map(
-            args.transcriber.process,
-            batched=True,
-            batch_size=args.batch_size,
+        dataset = load_from_disk(prev_stage_dir)
+
+        # Transcribe.
+        logger.info("Transcribing...")
+        dataset = dataset.map(
+            args.transcriber.process_sample,
+            fn_kwargs={"channel_name": channel_name},
         )
-        shard.save_to_disk(save_path)
+
+        # Filter out samples with empty transcripts.
+        logger.info("Filtering out samples with empty transcripts...")
+        dataset = dataset.filter(
+            lambda sample: sample["transcript"] is not None,
+            num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+        )
+
+        # Save dataset.
+        logger.info("Saving dataset...")
+        dataset.save_to_disk(
+            os.path.join(args.cur_stage_dir, channel_name),
+            num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+        )
 
 
 if __name__ == "__main__":
