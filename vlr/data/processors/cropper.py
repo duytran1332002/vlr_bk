2e2776545281682d5eb20688509af3c9b3494172
Thinh Duong <tanthinhdt24@gmail.com>
2023-10-07 00:00:41 +0700

    chore: update cropper and cropping task
    - add cropping task
    - write code in the channel-oriented way

--- vlr/data/processors/cropper.py
+++ vlr/data/processors/cropper.py
@@ -1,12 +1,84 @@
+import os
+import cv2
+import mediapipe as mp
 from vlr.data.processors.base import Processor
 
 
 class Cropper(Processor):
-    def __init__(self):
-        
-        
-    def process(self, batch: dict):
+    """
+    This class is used to crop mouth region.
+    """
+    def __init__(
+        self, mouth_dir: str,
+        min_detection_confidence: float = 0.9,
+        overwrite: bool = False,
+    ):
         """
-        
+        :param mouth_dir:                   Path to directory with mouth region.
+        :param min_detection_confidence:    Minimum confidence value for face detection.
+        :param overwrite:                   Overwrite existing files.
         """
-        return super().process()
+        self.mouth_dir = mouth_dir
+        self.face_detector = mp.solutions.face_detection.FaceDetection(
+            min_detection_confidence=min_detection_confidence
+        )
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.overwrite = overwrite
+
+    def process_sample(self, sample: dict, channel_name: str):
+        """
+        Crop mouth region.
+        :param batch:           Sample.
+        :param channel_name:    Channel name.
+        :return:                Sample with path to video of cropped mouth region.
+        """
+        id = sample["id"]
+        visual_path = sample["visual"]["path"]
+        fps = sample["visual"]["fps"]
+        mouth_path = os.path.join(self.mouth_dir, channel_name, f"{id}-mouth.mp4")
+
+        cap = cv2.VideoCapture(visual_path)
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+
+        one_face_only = True
+        frames = self.get_frames(cap)
+        for i, frame in enumerate(frames):
+            detections = self.face_detector.process(frame).detections
+            if detections and len(detections) == 1:
+                for detection in detections:
+                    bboxC = detection.location_data.relative_bounding_box
+                    ih, iw, _ = frame.shape
+                    x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                        int(bboxC.width * iw), int(bboxC.height * ih)
+                    frames[i] = frame[y + h//2:y + h, x:x + w]
+            else:
+                one_face_only = False
+                break
+
+        if one_face_only:
+            video_writer = cv2.VideoWriter(mouth_path, self.fourcc, fps, (frame_width, frame_height))
+            for frame in frames:
+                frame = cv2.resize(frame, (frame_width, frame_height))
+                video_writer.write(frame)
+            sample["visual"]["path"] = os.path.basename(mouth_path)
+            video_writer.release()
+        else:
+            sample["visual"]["path"] = None
+
+        cap.release()
+
+        return sample
+
+    def get_frames(self, cap: cv2.VideoCapture):
+        """
+        Get frames from sample.
+        :param cap:     Video capture.
+        """
+        frames = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frames.append(frame)
+        return frames
---
+++ vlr/data/tasks/crop.py
@@ -0,0 +1,87 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from tqdm import tqdm
+from dataclasses import dataclass
+from logging import getLogger
+from datasets import load_from_disk
+from vlr.data.utils import clean_up
+from vlr.data.processors.cropper import Cropper
+
+
+logger = getLogger()
+
+
+@dataclass
+class Args:
+    """
+    Data processing arguments.
+    """
+    # Path to directory of previous stage.
+    prev_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_3"
+    # Path to directory pf current stage.
+    cur_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_4"
+    # Path to directory containing mouth regions.
+    mouth_dir = "/mnt/d/Projects/sandboxes/vlr/mouths"
+    # Path to file containing channel names.
+    channel_names_path = "/mnt/d/Projects/sandboxes/vlr/channels.txt"
+
+    num_proc = 8
+    overwrite = True
+
+    cropper = Cropper(
+        mouth_dir=mouth_dir,
+        min_detection_confidence=0.99,
+        overwrite=overwrite,
+    )
+
+
+def main(args: Args):
+    """
+    Main function.
+    """
+    with open(args.channel_names_path, "r") as f:
+        channel_names = f.read().splitlines()
+
+    for channel_name in tqdm(
+        channel_names,
+        desc="Processing channels",
+        total=len(channel_names),
+        unit="channel"
+    ):
+        # Prepare save directory.
+        logger.info("Cleaning up old directories...")
+        clean_up(channel_name, [args.mouth_dir], args.overwrite)
+
+        # Get dataset.
+        logger.info("Preparing dataset...")
+        prev_stage_dir = os.path.join(args.prev_stage_dir, channel_name)
+        if not os.path.exists(prev_stage_dir):
+            print(f"Channel {channel_name} does not exist.")
+            continue
+        dataset = load_from_disk(prev_stage_dir)
+
+        # Crop mouth regions.
+        dataset = dataset.map(
+            args.cropper.process_sample,
+            fn_kwargs={"channel_name": channel_name},
+        )
+
+        # Filter out samples with no mouth detected.
+        dataset = dataset.filter(
+            lambda sample: sample["visual"]["path"] is not None,
+            num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+        )
+
+        # Save dataset.
+        logger.info("Saving dataset...")
+        dataset.save_to_disk(
+            os.path.join(args.cur_stage_dir, channel_name),
+            num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
+        )
+
+
+if __name__ == "__main__":
+    main(Args())
