287065bb2755c73aed23b8c7da544f52af90fdd9
Thinh Duong <tanthinhdt24@gmail.com>
2023-10-06 23:58:01 +0700

    chore: update denoiser and denoising task
    - write code in the channel-oriented way

--- vlr/data/processors/denoiser.py
+++ vlr/data/processors/denoiser.py
@@ -1,35 +1,67 @@
+import os
 import torch
-import logging
+import torchaudio
+import numpy as np
+import soundfile as sf
 from denoiser import pretrained
 from denoiser.dsp import convert_audio
 from vlr.data.processors.base import Processor
-
-
-logger = logging.getLogger(__name__)
 
 
 class Denoiser(Processor):
     """
     This class is used to denoise audio array.
     """
-    def __init__(self):
+    def __init__(
+        self, denoised_dir: str = None,
+        sampling_rate: int = 16000,
+        overwrite: bool = False,
+    ):
+        """
+        :param denoised_dir:    Path to directory containing denoised sound files.
+        :param sampling_rate:   Sampling rate.
+        :param overwrite:       Overwrite existing files.
+        """
         self.model = pretrained.dns64().cuda()
+        self.denoised_dir = denoised_dir
+        self.sampling_rate = sampling_rate
+        self.resampler = torchaudio.transforms.Resample(
+            orig_freq=self.model.sample_rate,
+            new_freq=self.sampling_rate,
+        )
+        self.overwrite = overwrite
 
-    def process(self, batch: dict):
+    def process_sample(self, sample: dict, channel_name: str):
         """
         Denoise audio array.
-        :param batch:   Batch with audio array.
-        :return:        Samples with denoised audio array.
+        :param sample:          Sample.
+        :param channel_name:    Channel name.
+        :return:                Sample updated with path to denoised audio array.
         """
-        for i, audio_array in enumerate(batch["audio"]):
-            sampling_rate = batch["sampling_rate"][i]
+        id = sample["id"]
+        denoised_path = os.path.join(self.denoised_dir, channel_name, f"{id}-denoised.wav")
+
+        if self.overwrite or not os.path.exists(denoised_path):
+            audio_array, sampling_rate = torchaudio.load(sample["audio"])
             audio_array = convert_audio(
-                torch.tensor([audio_array]).cuda(),
+                audio_array.cuda(),
                 sampling_rate,
                 self.model.sample_rate,
                 self.model.chin
             )
+
             with torch.no_grad():
-                denoised_audio_array = self.model(audio_array[None])[0]
-            batch["audio"][i] = denoised_audio_array.data.cpu().numpy()
-        return batch
+                output = self.model(audio_array[None].float())
+            denoised_audio_array = self.resampler(output[0].cpu()).numpy()
+
+            sf.write(
+                denoised_path,
+                np.ravel(denoised_audio_array),
+                self.model.sample_rate,
+            )
+
+        sample["audio"] = {
+            "path": denoised_path,
+            "sampling_rate": self.sampling_rate,
+        }
+        return sample
--- vlr/data/tasks/denoise.py
+++ vlr/data/tasks/denoise.py
@@ -3,11 +3,12 @@ import sys
 
 sys.path.append(os.getcwd())
 
-import glob
 from dataclasses import dataclass
 from logging import getLogger
-from datasets import load_from_disk, Audio
+from datasets import load_from_disk
+from tqdm import tqdm
 from vlr.data.processors.denoiser import Denoiser
+from vlr.data.utils import clean_up
 
 
 logger = getLogger()
@@ -18,49 +19,63 @@ class Args:
     """
     Data processing arguments.
     """
-    data_dir = "/mnt/d/Projects/sandboxes/vlr/v0.2"
-    save_dir = "/mnt/d/Projects/sandboxes/vlr/v0.3"
-    sampling_rate = 16000
-    batch_size = 100
+    # Path to directory of previous stage.
+    prev_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_1"
+    # Path to directory pf current stage.
+    cur_stage_dir = "/mnt/d/Projects/sandboxes/vlr/stage_2"
+    # Path to directory containing denoised sound files.
+    denoised_dir = "/mnt/d/Projects/sandboxes/vlr/denoised"
+    # Path to file containing channel names.
+    channel_names_path = "/mnt/d/Projects/sandboxes/vlr/channels.txt"
+
     num_proc = 8
     overwrite = False
 
-    denoiser = Denoiser()
-
-
-def format(sample: dict):
-    """
-    Format sample.
-    :param sample:  Sample.
-    :return:        Formatted sample.
-    """
-    sample["sampling_rate"] = sample["audio"]["sampling_rate"]
-    sample["audio"] = sample["audio"]["array"]
-    return sample
+    denoiser = Denoiser(
+        denoised_dir=denoised_dir,
+        sampling_rate=16000,
+        overwrite=overwrite,
+    )
 
 
 def main(args: Args):
     """
     Main function.
     """
-    logger.info("Denoising audio...")
-    shard_paths = glob.glob(os.path.join(args.data_dir, "shard_*"))
-    for index, shard_path in enumerate(shard_paths):
-        save_path = os.path.join(args.save_dir, f"shard_{index}")
-        if os.path.exists(save_path) and not args.overwrite:
+    with open(args.channel_names_path, "r") as f:
+        channel_names = f.read().splitlines()
+
+    for channel_name in tqdm(
+        channel_names,
+        desc="Processing channels",
+        total=len(channel_names),
+        unit="channel"
+    ):
+        # Prepare save directory.
+        logger.info("Cleaning up old directories...")
+        clean_up(channel_name, [args.denoised_dir], args.overwrite)
+
+        # Get dataset.
+        logger.info("Preparing dataset...")
+        prev_stage_dir = os.path.join(args.prev_stage_dir, channel_name)
+        if not os.path.exists(prev_stage_dir):
+            print(f"Channel {channel_name} does not exist.")
             continue
-        shard = load_from_disk(shard_path)
-        shard = shard.cast_column("audio", Audio(sampling_rate=args.sampling_rate))
-        shard = shard.map(
-            format,
-            num_proc=args.num_proc if args.num_proc <= os.cpu_count() else os.cpu_count(),
+        dataset = load_from_disk(prev_stage_dir)
+
+        # Denoise audio.
+        logger.info("Denoising audio...")
+        dataset = dataset.map(
+            args.denoiser.process_sample,
+            fn_kwargs={"channel_name": channel_name},
         )
-        shard = shard.map(
-            args.denoiser.process,
-            batched=True,
-            batch_size=args.batch_size,
+
+        # Save dataset.
+        logger.info("Saving dataset...")
+        dataset.save_to_disk(
+            os.path.join(args.cur_stage_dir, channel_name),
+            num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
         )
-        shard.save_to_disk(save_path)
 
 
 if __name__ == "__main__":
