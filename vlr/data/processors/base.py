28900975a0b55b58451de4a80c89db88c9bc6475
Thinh Duong <tanthinhdt24@gmail.com>
2023-10-06 23:55:48 +0700

    chore: update base processor
    - specify function process for sample and batch

--- vlr/data/processors/base.py
+++ vlr/data/processors/base.py
@@ -2,8 +2,16 @@ class Processor:
     """
     This class and its children are used to perform specific tasks to process data.
     """
-    def process(self):
+    def process_sample(self, sample: dict):
         """
-        Process data.
+        Process sample.
+        :param sample:  Sample.
+        :return:        Processed sample.
+        """
+        raise NotImplementedError("Please implement this method in child classes")
+
+    def process_batch(self):
+        """
+        Process batch of data.
         """
         raise NotImplementedError("Please implement this method in child classes")
