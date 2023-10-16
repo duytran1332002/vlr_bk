75f7a9ec102476820db6e8d4b723cd2516b83d9b
duytran <crackonebrain@gmail.com>
2023-10-16 10:44:13 +0700

    chore: add avr_checkpoint to save model utils

--- vlr/utils/upload_file_to_hf.py
+++ vlr/utils/upload_file_to_hf.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     # By default, files are uploaded at the root of the repo
 
     api.upload_file(
-        path_or_fileobj="/home/duytran/Downloads/New_raw_data_lip_reading/vtv24news_output.zip",
-        path_in_repo="active_speaker/vtv24news_output.zip",
+        path_or_fileobj="/home/duytran/Downloads/vlr/active_speaker/ha_hips_output.zip",
+        path_in_repo="active_speaker/ha_hips_output.zip",
         repo_id="fptu/vlr",
         repo_type="dataset",
     )
