49faa0ac4ef5595ce0e8411b3ba7acbd222f60af
duytran <crackonebrain@gmail.com>
2023-11-15 20:44:24 +0700

    chore: add demo and download video scripts

--- vlr/scripts/eval.sh
+++ vlr/scripts/eval.sh
@@ -1,1 +1,5 @@
-python vlr/eval.py
+python vlr/eval.py  data.dataset.root_dir=/home/duytran/Downloads/vlr \
+                    data.dataset.test_size=0.01 \
+                    data.num_workers=15 \
+
+ 
