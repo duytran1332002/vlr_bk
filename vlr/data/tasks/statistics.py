bce297f11437d1a4cf1f6ab391be360105e8979c
Thinh Duong <tanthinhdt24@gmail.com>
2023-11-24 11:19:39 +0700

    chore: update statistics task

--- vlr/data/tasks/statistics.py
+++ vlr/data/tasks/statistics.py
@@ -7,7 +7,7 @@ import polars as pl
 from tqdm import tqdm
 from dataclasses import dataclass
 from logging import getLogger
-from datasets import load_from_disk
+from datasets import load_dataset
 
 
 logger = getLogger()
@@ -19,13 +19,13 @@ class Args:
     Data processing arguments.
     """
     # Path to directory of the final stage.
-    final_stage_dir = "/mnt/d/Projects/sandboxes/vlr/cropping"   # Change this.
+    final_stage_dir = "/mnt/d/Projects/sandboxes/vlr/v1/stage_4"   # Change this.
     # Path to directory containing transcripts.
-    transcript_dir = "/mnt/d/Projects/sandboxes/vlr/transcripts"    # Change this.
+    transcript_dir = "/mnt/d/Projects/sandboxes/vlr/v1/transcripts"    # Change this.
     # Path to directory containing statistics.
-    statistics_dir = "/mnt/d/Projects/sandboxes/vlr/statistics"   # Change this.
+    statistics_dir = "/mnt/d/Projects/sandboxes/vlr/v1/statistics"   # Change this.
     # Path to file containing channel names.
-    channel_names_path = "/mnt/d/Projects/sandboxes/vlr/done.txt"   # Change this.
+    channel_names_path = "/mnt/d/Projects/sandboxes/vlr/channels.txt"   # Change this.
 
 
 def main(args: Args):
@@ -36,6 +36,7 @@ def main(args: Args):
         channel_names = f.read().strip().split()
 
     dictionary = {}
+    total_samples = 0
     total_duration = 0
     for channel_name in tqdm(
         channel_names,
@@ -45,11 +46,11 @@ def main(args: Args):
     ):
         # Get dataset.
         logger.info("Preparing dataset...")
-        prev_stage_dir = os.path.join(args.final_stage_dir, channel_name)
+        prev_stage_dir = os.path.join(args.final_stage_dir, channel_name + ".json")
         if not os.path.exists(prev_stage_dir):
             print(f"Channel {channel_name} does not exist.")
             continue
-        dataset = load_from_disk(prev_stage_dir)
+        dataset = load_dataset("json", data_files=prev_stage_dir, split="train")
 
         # Do statistics on dataset.
         logger.info("Doing statistics dataset...")
@@ -59,12 +60,14 @@ def main(args: Args):
             total=dataset.num_rows,
             unit="sample"
         ):
-            transcript_path = os.path.join(args.transcript_dir, channel_name, sample["transcript"])
+            transcript_path = os.path.join(args.transcript_dir, channel_name, sample["id"] + ".txt")
             with open(transcript_path, "r") as f:
                 words = f.read().split()
             for word in words:
                 dictionary[word] = dictionary.get(word, 0) + 1
             total_duration += sample["duration"]
+
+        total_samples += dataset.num_rows
 
     # Save statistics.
     logger.info("Saving statistics...")
@@ -79,6 +82,7 @@ def main(args: Args):
     words_df.write_csv(os.path.join(args.statistics_dir, "words.csv"))
 
     with open(os.path.join(args.statistics_dir, "statistics.txt"), 'w') as f:
+        print(f"Number of samples: {total_samples}", file=f)
         print(f"Number of vocabularies: {len(dictionary)}", file=f)
         print(f"Total number of words: {sum(dictionary.values())}", file=f)
         print(f"Total duration: {total_duration}s", file=f)
