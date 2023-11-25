f8e6e11c9b6b26dbeb69caf40c8c8d9375433a3e
Thinh Duong <tanthinhdt24@gmail.com>
2023-11-25 11:31:00 +0700

    chore: add downloading from huggingface

---
+++ vlr/scripts/download_from_hf.sh
@@ -0,0 +1,5 @@
+python vlr/data/tasks/download.py \
+            --token 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 \
+            --subfolder v1/stage_2 \
+            --channel-names-path /mnt/d/Projects/sandboxes/vlr/channels.txt \
+            --dest /mnt/d/Projects/sandboxes/vlr
