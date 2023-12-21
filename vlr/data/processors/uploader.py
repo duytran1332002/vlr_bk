d1b6c65d32f8beb1f2e1acaa4f6098202efd97b9
Thinh Duong <tanthinhdt24@gmail.com>
2023-12-21 22:17:41 +0700

    chore: add new processor - uploader
    - uploader is in charge of uploading files and folders to HuggingFace

---
+++ vlr/data/processors/uploader.py
@@ -0,0 +1,57 @@
+import os
+from huggingface_hub import HfApi, HfFolder
+from vlr.data.utils import zip_dir
+from .processor import Processor
+
+
+class Uploader(Processor):
+    TOKEN = HfFolder.get_token()
+
+    def __init__(self) -> None:
+        """
+        Initialize processor.
+        """
+        self.api = HfApi()
+
+    def zip_and_upload_dir(
+        self, dir_path: str,
+        repo_id: str,
+        path_in_repo: str,
+        repo_type: str = "dataset",
+    ) -> None:
+        """
+        Zip directory and upload it to the hub.
+        :param dir_path:        Path to directory.
+        :param repo_id:         Repository id.
+        :param path_in_repo:    Path to directory in repository.
+        :param repo_type:       Repository type.
+        """
+        self.api.upload_file(
+            path_or_fileobj=zip_dir(dir_path, overwrite=True),
+            path_in_repo=path_in_repo,
+            repo_id=repo_id,
+            token=self.TOKEN,
+            repo_type=repo_type,
+        )
+        os.remove(dir_path + ".zip")
+
+    def upload_file(
+        self, file_path: str,
+        repo_id: str,
+        path_in_repo: str,
+        repo_type: str = "dataset",
+    ) -> None:
+        """
+        Upload file to the hub.
+        :param file_path:       Path to file.
+        :param repo_id:         Repository id.
+        :param path_in_repo:    Path to file in repository.
+        :param repo_type:       Repository type.
+        """
+        self.api.upload_file(
+            path_or_fileobj=file_path,
+            path_in_repo=path_in_repo,
+            repo_id=repo_id,
+            token=self.TOKEN,
+            repo_type=repo_type,
+        )
