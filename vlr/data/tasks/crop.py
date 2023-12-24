4fd166681490f9eed190d93a2ee6619e99d577b0
Thinh Duong <tanthinhdt24@gmail.com>
2023-12-24 19:39:58 +0700

    chore: update tasks

--- vlr/data/tasks/crop.py
+++ vlr/data/tasks/crop.py
@@ -5,18 +5,14 @@ sys.path.append(os.getcwd())
 
 import argparse
 from tqdm import tqdm
-from datasets import load_dataset, disable_caching
-from vlr.data.utils.tools import clean_up
-from vlr.data.processors.cropper import Cropper
-
-
-disable_caching()
+from vlr.data.utils.file_system import prepare_dir
+from vlr.data.processors import Executor
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--data-dir",
+        "--output-dir",
         type=str,
         required=True,
         help="Path to data directory.",
@@ -28,101 +24,104 @@ def parse_args() -> argparse.Namespace:
         help="Path to file containing channel names.",
     )
     parser.add_argument(
-        "--num-proc",
+        "--version",
         type=int,
-        default=-1,
-        help="Number of processes.",
+        default=1,
+        help="version of the dataset.",
     )
     parser.add_argument(
         "--overwrite",
-        type=bool,
         action=argparse.BooleanOptionalAction,
+        default=False,
         help="Overwrite existing files.",
+    )
+    parser.add_argument(
+        "--upload-to-hub",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Upload to hub after processing.",
+    )
+    parser.add_argument(
+        "--clean-up",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Clean up all downloaded files after processing.",
     )
     return parser.parse_args()
 
 
 def main(args: argparse.Namespace) -> None:
     """
-    Main function.
+    This function is used to crop mouth regions from videos.
+    :param args:    Arguments from command line.
     """
-    if not os.path.exists(args.data_dir):
-        print(f"Directory {args.data_dir} does not exist.")
-        return
-    visual_dir = os.path.join(args.data_dir, "visual")
-    if not os.path.exists(visual_dir):
-        print(f"Directory {visual_dir} does not exist.")
-        return
-    prev_stage_dir = os.path.join(args.data_dir, "stage_4")
-    if not os.path.exists(prev_stage_dir):
-        print(f"Directory {prev_stage_dir} does not exist.")
-        return
-
-    mouth_dir = os.path.join(args.data_dir, "mouths")
-    os.makedirs(mouth_dir, exist_ok=True)
-    cur_stage_dir = os.path.join(args.data_dir, "stage_5")
-    os.makedirs(cur_stage_dir, exist_ok=True)
-
-    if args.channel_names_path:
-        with open(args.channel_names_path, "r") as f:
-            channel_names = f.read().strip().split()
-    else:
-        channel_names = os.listdir(visual_dir)
-
-    cropper = Cropper(
-        visual_dir=visual_dir,
-        mouth_dir=mouth_dir,
-        padding=10,
+    print("Initialize executor...")
+    executor = Executor(
+        processor_name="cropper",
+        src_repo_id="fptu/vietnamese-speaker-clip",
+        dest_repo_id="fptu/vietnamese-speaker-lip-clip",
+        output_dir=args.output_dir,
+    )
+    executor = executor.load_channels(
+        channel_names_to_process_path=args.channel_names_path,
         overwrite=args.overwrite,
     )
 
-    print("\n" + "#" * 50 + " Cropping " + "#" * 50)
-    for channel_name in tqdm(
-        channel_names,
+    for channel in tqdm(
+        executor.available_channels,
         desc="Processing channels",
-        total=len(channel_names),
+        total=len(executor.available_channels),
         unit="channel"
     ):
-        print("-" * 20 + f" Processing {channel_name} " + "-" * 20)
+        print("-" * 20 + f" Processing {channel} " + "-" * 20)
+
         # Prepare save directory.
-        clean_up(channel_name, [mouth_dir], args.overwrite)
+        print("Preparing save directory...")
+        channel_visual_dir = prepare_dir(
+            dir=os.path.join(args.output_dir, "visual", channel),
+            overwrite=args.overwrite,
+        )
 
         # Get dataset.
         print("Preparing dataset...")
-        prev_stage_path = os.path.join(prev_stage_dir, channel_name + ".json")
-        if not os.path.exists(prev_stage_path):
-            print(f"Channel {channel_name} does not exist.")
-            continue
-        dataset = load_dataset(
-            "json", data_files=prev_stage_path, split="train",
+        executor = executor.load_dataset(
+            channel=channel,
+            remove_columns=["audio"],
         )
-        num_samples_before = dataset.num_rows
 
         # Crop mouth regions.
-        dataset = dataset.map(
-            cropper.process_sample,
+        print("Cropping mouth regions...")
+        executor = executor.process_sample(
+            fn_kwargs={
+                "visual_output_dir": channel_visual_dir,
+                "padding": 20,
+            },
+            remove_columns=["visual"],
         )
-
-        # Filter out samples with no mouth detected.
-        dataset = dataset.filter(
-            lambda sample: sample["id"] is not None,
-            num_proc=args.num_proc if 0 < args.num_proc <= os.cpu_count() else os.cpu_count(),
-        )
-        num_samples_after = dataset.num_rows
 
         # Check number of samples.
-        assert len(os.listdir(os.path.join(mouth_dir, channel_name))) == dataset.num_rows, \
-            f"{channel_name} - Number of mouth regions does not match that in dataset."
-        print(f"Number of samples lost: {num_samples_before - num_samples_after}")
+        print("Checking number of samples...")
+        executor.check_num_samples_in_dir(channel_visual_dir)
+        print(f"\tNumber of samples lost: {executor.get_num_samples_change()}")
 
         # Save dataset.
-        print("Saving dataset...")
-        dataset.to_pandas().to_json(
-            os.path.join(cur_stage_dir, channel_name + ".json"),
-            orient="records",
-        )
-        dataset.cleanup_cache_files()
-        print("-" * (13 + len(channel_name) + 2 * 20))
+        print("Saving metada...")
+        executor.save_metadata(channel)
+
+        # Upload to hub.
+        if args.upload_to_hub:
+            print("Uploading to hub...")
+            executor.upload_metadata_to_hub(channel=channel)
+            executor.zip_and_upload_dir(
+                dir_path=channel_visual_dir,
+                path_in_repo=os.path.join("visual", channel + ".zip"),
+            )
+
+        print("-" * (13 + len(channel) + 2 * 20))
+
+    if args.clean_up:
+        print("Cleaning up...")
+        executor.clean_cache()
 
 
 if __name__ == "__main__":
--- vlr/data/tasks/denoise.py
+++ vlr/data/tasks/denoise.py
@@ -4,19 +4,15 @@ import sys
 sys.path.append(os.getcwd())
 
 import argparse
-from datasets import load_dataset, disable_caching
 from tqdm import tqdm
-from vlr.data.processors.denoiser import Denoiser
-from vlr.data.utils.tools import clean_up
-
-
-disable_caching()
+from vlr.data.processors import Executor
+from vlr.data.utils import prepare_dir
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--data-dir",
+        "--output-dir",
         type=str,
         required=True,
         help="Path to data directory.",
@@ -28,16 +24,28 @@ def parse_args() -> argparse.Namespace:
         help="Path to file containing channel names.",
     )
     parser.add_argument(
-        "--batch-size",
+        "--version",
         type=int,
-        default=100,
-        help="Batch size.",
+        default=1,
+        help="version of the dataset.",
     )
     parser.add_argument(
         "--overwrite",
         action=argparse.BooleanOptionalAction,
         default=False,
         help="Overwrite existing files.",
+    )
+    parser.add_argument(
+        "--upload-to-hub",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Upload to hub after processing.",
+    )
+    parser.add_argument(
+        "--clean-up",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Upload to hub after processing.",
     )
     return parser.parse_args()
 
@@ -46,80 +54,73 @@ def main(args: argparse.Namespace):
     """
     Main function.
     """
-    if not os.path.exists(args.data_dir):
-        print(f"Directory {args.data_dir} does not exist.")
-        return
-    audio_dir = os.path.join(args.data_dir, "audio")
-    if not os.path.exists(audio_dir):
-        print(f"Directory {audio_dir} does not exist.")
-        return
-    prev_stage_dir = os.path.join(args.data_dir, "stage_1")
-    if not os.path.exists(prev_stage_dir):
-        print(f"Directory {prev_stage_dir} does not exist.")
-        return
-
-    denoised_dir = os.path.join(args.data_dir, "denoised")
-    os.makedirs(denoised_dir, exist_ok=True)
-    cur_stage_dir = os.path.join(args.data_dir, "stage_2")
-    os.makedirs(cur_stage_dir, exist_ok=True)
-
-    if args.channel_names_path:
-        with open(args.channel_names_path, "r") as f:
-            channel_names = f.read().strip().split()
-    else:
-        channel_names = os.listdir(audio_dir)
-
-    denoiser = Denoiser(
-        audio_dir=audio_dir,
-        denoised_dir=denoised_dir,
-        sampling_rate=16000,
+    print("Initialize executor...")
+    executor = Executor(
+        processor_name="denoiser",
+        src_repo_id="fptu/vietnamese-speaker-lip-clip",
+        dest_repo_id="fptu/denoised-vietnamese-audio",
+        output_dir=args.output_dir,
         overwrite=args.overwrite,
     )
+    executor = executor.load_channels(
+        channel_names_to_process_path=args.channel_names_path,
+    )
 
-    print("\n" + "#" * 50 + " Denoising " + "#" * 50)
-    for channel_name in tqdm(
-        channel_names,
+    for channel in tqdm(
+        executor.available_channels,
         desc="Processing channels",
-        total=len(channel_names),
+        total=len(executor.available_channels),
         unit="channel"
     ):
-        print("-" * 20 + f" Processing {channel_name} " + "-" * 20)
+        print("-" * 20 + f" Processing {channel} " + "-" * 20)
+
         # Prepare save directory.
-        clean_up(channel_name, [denoised_dir], args.overwrite)
+        print("Preparing save directory...")
+        channel_audio_dir = prepare_dir(
+            dir=os.path.join(args.output_dir, "audio", channel),
+            overwrite=args.overwrite,
+        )
 
         # Get dataset.
-        print("Preparing dataset...")
-        prev_stage_path = os.path.join(prev_stage_dir, channel_name + ".json")
-        if not os.path.exists(prev_stage_path):
-            print(f"Channel {channel_name} does not exist.")
-            continue
-        dataset = load_dataset(
-            "json", data_files=prev_stage_path, split="train",
+        print("Loading dataset...")
+        executor = executor.load_dataset(
+            channel=channel,
+            remove_columns=["visual"],
         )
-        num_samples_before = dataset.num_rows
 
         # Denoise audio.
         print("Denoising audio...")
-        dataset = dataset.map(
-            denoiser.process_batch,
-            batched=True,
-            batch_size=args.batch_size,
+        executor = executor.process_sample(
+            fn_kwargs={
+                "audio_output_dir": channel_audio_dir,
+                "output_sampling_rate": 16000,
+            },
+            remove_columns=["audio"],
         )
-        num_samples_after = dataset.num_rows
 
         # Check number of samples.
-        assert len(os.listdir(os.path.join(denoised_dir, channel_name))) == dataset.num_rows, \
-            f"{channel_name} - Number of denoised samples does not match that in dataset."
-        print(f"Number of samples lost: {num_samples_before - num_samples_after}")
+        print("Checking number of samples...")
+        executor.check_num_samples_in_dir(channel_audio_dir)
+        print(f"\tNumber of samples lost: {executor.get_num_samples_change()}")
 
-        # Save dataset.
-        print("Saving dataset...")
-        dataset.to_pandas().to_json(
-            os.path.join(cur_stage_dir, channel_name + ".json"),
-            orient="records",
-        )
-        dataset.cleanup_cache_files()
-        print("-" * (13 + len(channel_name) + 2 * 20))
+        # Save metadata.
+        print("Saving metada...")
+        executor.save_metadata(channel)
+
+        # Upload to hub.
+        if args.upload_to_hub:
+            print("Uploading to hub...")
+            executor.upload_metadata_to_hub(channel=channel)
+            executor.zip_and_upload_dir(
+                dir_path=channel_audio_dir,
+                path_in_repo=os.path.join("audio", channel + ".zip"),
+            )
+
+        print("-" * (13 + len(channel) + 2 * 20))
+
+    if args.clean_up:
+        print("Cleaning up...")
+        executor.clean_cache()
 
 
 if __name__ == "__main__":
